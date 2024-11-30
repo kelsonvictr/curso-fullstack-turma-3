e9f061cdac6db1655bcc87ac95b59b0bf842808b
kelsonalmeida <kelson.victr@gmail.com>
2024-11-30 16:26:06 -0300

    sobreNos

---
+++ techhub-site/src/pages/SobreNos.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SobreNos = () => {
+  return (
+    <div>
+        <h1>Sobre Nós</h1>
+        <p>O TechHub é dedicado a trazer as últimas notícias sobre tecnologia... Bla Bla Bla...</p>
+    </div>
+  )
+}
+
+export default SobreNos
